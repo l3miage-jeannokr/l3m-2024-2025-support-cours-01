faebb36eb7658d332de50dc7b121b5d8237750aa
AlBob <>
2024-09-20 09:26:33 +0200

    ++

---
+++ src/app/data/college.tuple.ts
@@ -0,0 +1,12 @@
+// exemple : 2020;GRENOBLE;ARDECHE;ANNONAY;COLLEGE PRIVE;SACRE COEUR;PRIVE;0;0;712;168;80;88;0;193;07100
+
+export type DataCollege = [
+    rentree_scolaire: number,
+    denomination_principale: string,
+    patronyme: string,
+    secteur: string, // à discuter
+    rep: boolean,
+    rep_plus: boolean,
+    nombre_eleves_total: number,
+    code_postal: string // ou number ?
+]
