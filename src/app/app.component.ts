4337ac4b52ccb77d4d796508db0bd79da753fe8f
alexandre.demeure@univ-grenoble-alpes.fr <alexandre.demeure@univ-grenoble-alpes.fr>
2024-12-30 11:18:36 +0100

    --> 19

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,10 @@ import { RouterOutlet } from '@angular/router';
 import { DataProcessService } from './services/data-process.service';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    imports: [RouterOutlet],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss'
 })
 export class AppComponent {
   readonly dataProcessSrv = inject(DataProcessService);
